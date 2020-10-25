800852c16b0fc1b4f0a35559ecdd69df0ca0e20e
Jens Cherukad <jenscherukad@gmail.com>
2020-10-25 20:26:33 +0100

    accordion fix, color change, content
    Added some content,
    Accordion works correct,
    Color change

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+    let panel = this.querySelector('.panel');;
+    if (panel.style.height == 'auto') {
+      panel.style.height = '0px';
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.height = 'auto';
     }
   });
 }
